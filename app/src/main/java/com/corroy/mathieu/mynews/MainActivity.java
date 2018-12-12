c3656a1bffbb98a169be7b0145bf5fc81c716b95
Mathieu72000 <Math.corroy@gmail.com>
2018-12-12 17:42:51 +0100

    Added 3 new methods : - configureToolbar() - configureDrawerLayout() - configureNavigationView()
    and call them on the onCreate() method to configure the Navigation Drawer
    
    Added onNavigationItemSelected() method to handle the navigation item click.
    
    Added an onBackPressed() method to handle the back click to close the menu

--- app/src/main/java/com/corroy/mathieu/mynews/MainActivity.java
+++ app/src/main/java/com/corroy/mathieu/mynews/MainActivity.java
@@ -1,13 +1,86 @@
 package com.corroy.mathieu.mynews;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    // FOR DESIGN
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.configureToolbar();
+
+        this.configureDrawerLayout();
+
+        this.configureNavigationView();
+    }
+
+    @Override
+    public void onBackPressed(){
+        // Handle back click to close menu
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+
+        int id = item.getItemId();
+
+        // Handle Navigation Item Click
+        switch (id){
+            case R.id.topStories:
+                break;
+            case R.id.mostPopular:
+                break;
+            case R.id.politics:
+                break;
+            default:
+                break;
+        }
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    // ----------------
+    // CONFIGURATION
+    // ----------------
+
+    // Configure Toolbar
+    private void configureToolbar() {
+        this.toolbar = findViewById(R.id.activityMainToolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    // Configure DrawerLayout
+    private void configureDrawerLayout() {
+        this.drawerLayout = findViewById(R.id.activityMainDrawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // Configure NavigationView
+    private void configureNavigationView(){
+       this.navigationView = findViewById(R.id.activityMainNavigationView);
+       navigationView.setNavigationItemSelectedListener(this);
     }
 }
+
